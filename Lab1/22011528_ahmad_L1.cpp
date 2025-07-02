bd4fd7b0e4d443c821e0225343b744e430c10b71
Ahmad Waffy Rez'Daniel Bin Ahmad Rizal <uxncode.dev@gmail.com>
2025-07-02 09:12:18 +0800

    Add files via upload

---
+++ Lab1/22011528_ahmad_L1.cpp
@@ -0,0 +1,51 @@
+kkkk/* 
+*************************
+Id : 22011528
+Name : Ahmad Waffy Rez'Daniel Bin Ahmad Rizal
+Lab : 1
+*************************
+*/
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct Student {
+    string name;
+    int id;
+    string phone;
+    string email;
+}; 
+
+int main() {
+    Student stud[5];
+
+    for (int i = 0; i < 5; i++) {
+        cout << "Enter details for student " << i + 1 << ":" << endl;
+        
+        cout << "Name: ";
+        getline(cin >> ws, stud[i].name);
+
+        cout << "ID: ";
+        cin >> stud[i].id;
+
+        cout << "Phone: ";
+        cin >> stud[i].phone;
+
+        cout << "Email: ";
+        cin >> stud[i].email;
+
+        cout << endl;
+    }
+    
+    cout << endl << "Student details:" << endl;
+    for (int i = 0; i < 5; i++) {
+        cout << "Student " << i + 1 << ":" << endl;
+        cout << "Name: " << stud[i].name << endl;
+        cout << "ID: " << stud[i].id << endl;
+        cout << "Phone: " << stud[i].phone << endl;
+        cout << "Email: " << stud[i].email << endl << endl;
+    }
+
+    return 0;
+}
